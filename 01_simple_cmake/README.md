e59ac972ee510f47a5d037bc96e7e5475acdc80e
Hiroki Yamaguchi <hiroki6357@gmail.com>
2018-05-20 11:35:13 +0900

    add 01_simple_cmake

---
+++ 01_simple_cmake/README.md
@@ -0,0 +1,4 @@
+# 参考
+- [CMake チュートリアル](http://opencv.jp/cmake/cmake_tutorial.html)
+- [cmake チュートリアルを読む前に](https://qiita.com/maueki/items/9cce88f6122e205754c5)
+
---
+++ 01_simple_cmake/tutorial.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "tutorial.h"
+
+int main(void) {
+	std::cout << "Hello World!! " << "version:" << VERSION << std::endl;
+
+	return 0;
+}
